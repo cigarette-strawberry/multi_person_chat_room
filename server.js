e4c901b0eb584f202bd2cd77730460ff2ab3d972
wu0304 <2467337566@qq.com>
2023-06-07 23:12:19 +0800

    feat:首次提交

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs')
+const path = require('path');
+const mime = require('mime');
+const cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'})
+    response.write('Error 404:response not found')
+    response.end()
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {'content-type': mime.getType(path.basename(filePath))})
+    response.end(fileContents)
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) sendFile(response, absPath, cache[absPath])
+    else {
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+                fs.readFile(absPath, function (err, data) {
+                    if (err) send404(response)
+                    else {
+                        cache[absPath] = data
+                        sendFile(response, absPath, data)
+                    }
+                })
+            } else send404(response)
+        })
+    }
+}
+
+let server = http.createServer(function (request, response) {
+    let filePath = false
+    if (request.url == '/') filePath = 'public/index.html'
+    else filePath = 'public' + request.url
+    const absPath = './' + filePath
+    serveStatic(response, cache, absPath)
+})
+
+server.listen(3000, function () {
+    console.log('Server listening on port 3000.')
+})
+
+var chatServer = require('/lib/chat_server')
+chatServer.listen(server)
